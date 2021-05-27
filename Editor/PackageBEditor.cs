f757b8920c2a9e779d363d767a408fa21b2f5566
thomasbousquet <tom.bousquet@gmail.com>
2021-05-27 03:08:23 -0400

    setup test classes

--- Editor/PackageBEditor.cs
+++ Editor/PackageBEditor.cs
@@ -2,8 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PackageBEditor : MonoBehaviour
+public class PackageBEditor : PackageARuntimeTest
 {
+
     // Start is called before the first frame update
     void Start()
     {
